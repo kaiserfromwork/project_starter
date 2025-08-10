0cb87f210bade056e43c6778127b988f847bffc3
Kaiser <kaiserfromwork@gmail.com>
2025-08-10 21:33:18 +0100

    This is a commit!

--- commit_changes.py
+++ commit_changes.py
@@ -1,14 +1,18 @@
 import subprocess
 
-
+print("Trying to git add .")
 add_command = subprocess.run(
     ["git", "add", "."], check=True, capture_output=True, text=True
 )
+
 # TODO: ADD THE OPTION TO CREATE A CUSOTM MESSAGE
+print("Trying to commit with a message")
 message = "This is a commit!"
 commit_command = subprocess.run(
     ["git", "commit", "-m", message], check=True, capture_output=True, text=True
 )
+
+print("Trying to push")
 push_command = subprocess.run(
     ["git", "push"], check=True, capture_output=True, text=True
 )
